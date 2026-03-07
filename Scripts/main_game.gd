7c2bae960d8cc60fe115e0ab763b2268cd697a54
Ashmit here <ashmitagnihotrihero@gmail.com>
2026-03-07 16:49:11 +0530

    updated pipes and more things

--- Scripts/main_game.gd
+++ Scripts/main_game.gd
@@ -1,14 +1,10 @@
 extends Node2D
 
-var Pipe_scene = preload("res://Scenes/pipe.tscn")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 
 func pipe_spawner() -> void:
+	var Pipe_scene = load("res://Scenes/pipe.tscn")
 	var pipe = Pipe_scene.instantiate()
-	pipe.position = Vector2(400,randi_range(120,600))
+	pipe.position = Vector2(1200,randi_range(120,600))
 	$Pipes.add_child(pipe)
