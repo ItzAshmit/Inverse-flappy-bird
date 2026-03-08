2001fe1bb0d8f36fbd7c01d41631988df4c54827
smkillerz777-star <s.mkillerz777@gmail.com>
2026-03-08 10:36:23 +0530

    added pipe spwaner by player
    now pipe will spawn at the position that the user want will a cooldown

--- Scripts/main_game.gd
+++ Scripts/main_game.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-
-
-
 func pipe_spawner() -> void:
 	var Pipe_scene = load("res://Scenes/pipe.tscn")
-	print(Pipe_scene)
 	var pipe = Pipe_scene.instantiate()
-	pipe.position = Vector2(1200,randi_range(275,425))
+	pipe.global_position = get_global_mouse_position()
 	$pipes.add_child(pipe)
+	
+func _input(event):
+	if event is InputEventMouseButton and $"Timer/PipeSpawner".time_left==0:
+		$Timer/PipeSpawner.start()
+		pipe_spawner()
--- Scripts/pipe.gd
+++ Scripts/pipe.gd
@@ -9,5 +9,3 @@ func _process(delta: float) -> void:
 
 func _on_upper_body_entered(_body: Node2D) -> void:
 	get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
-
-
