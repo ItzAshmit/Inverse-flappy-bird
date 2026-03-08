06b1f5e3178ea9ec1fa028c897f88781ae0073b0
Ashmit here <ashmitagnihotrihero@gmail.com>
2026-03-08 11:57:46 +0530

    idk what did i do but i did something

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
-var speed= 250
-var gravity = 10
+var speed:int = 250
+var gravity:int = 10
+var has_pipes_to_dogde:bool = false
 func _ready() -> void:
 	velocity =  Vector2.UP*speed
 
@@ -11,10 +12,19 @@ func jump():
 
 
 
-
 func _physics_process(_delta: float) -> void:
 	velocity.y += gravity
-	move_and_slide()
-	if($Timer.time_left==0): 
+	
+	if $RayCast2D.is_colliding():
+		has_pipes_to_dogde = true
+		if $RayCast2D.get_collider().collision_layer == 4:
+			jump()
+
+
+	if($Timer.time_left==0) and not has_pipes_to_dogde:
 		jump()
 		$Timer.start()
+
+	elif has_pipes_to_dogde:
+		pass
+	move_and_slide()
