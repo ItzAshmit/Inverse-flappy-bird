ffdece03b09a9d3f72a3c8138747891d65a0f549
Ashmit here <ashmitagnihotrihero@gmail.com>
2026-03-08 12:32:18 +0530

    dodging pipes

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -19,12 +19,17 @@ func _physics_process(_delta: float) -> void:
 		has_pipes_to_dogde = true
 		if $RayCast2D.get_collider().collision_layer == 4:
 			jump()
+			has_pipes_to_dogde = false
+	
+	if $RayCast2D2.is_colliding():
+		has_pipes_to_dogde = true
+
+	if $RayCast2D3.is_colliding() and not $RayCast2D2.is_colliding():
+		jump()
 
 
 	if($Timer.time_left==0) and not has_pipes_to_dogde:
 		jump()
 		$Timer.start()
 
-	elif has_pipes_to_dogde:
-		pass
 	move_and_slide()
