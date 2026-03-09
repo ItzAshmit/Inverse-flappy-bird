26dde412a10eecf66f73633f9d5215adb06cfdf6
smkillerz777-star <s.mkillerz777@gmail.com>
2026-03-09 16:43:52 +0530

    Adjust raycasts and add layer-aware collisions
    Move a border CollisionShape2D and shorten several RayCast2D target_positions (front, front-below, below, below2) to tighten detection ranges. Add get_collider_layer(ray) helper to safely read a collider's collision_layer and update _physics_process to use layer-aware checks for jumping/diving decisions. Also adjust control flow and add debug prints to clarify collision branches and prevent unintended jumps when colliding with specific layers.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -15,15 +15,22 @@ func jump(out):
 	goingdown = false
 	print(out)
 
+func get_collider_layer(ray) -> int:
+	if ray.is_colliding():
+		return ray.get_collider().collision_layer
+	return -1
+
 func _physics_process(_delta: float) -> void:
 	jumped = false
 	velocity.y += gravity
-	if $above.is_colliding() or $above2.is_colliding():
+	if $below.is_colliding() or $below2.is_colliding() and (get_collider_layer($below) == 4 or get_collider_layer($below2) == 4):
+		goingdown=false
+		jump("3")
+	elif ($above.is_colliding() or $above2.is_colliding()) and (get_collider_layer($above) == 2 or get_collider_layer($above2) == 2):
 		goingdown=true
 		diving("c")
-	elif $below.is_colliding() or $below2.is_colliding():
-		jump("3")
 	else:
+		print("k")
 		if $front.is_colliding():
 			print($front.get_collider())
 			if $front.get_collider().collision_layer == 2:
@@ -34,13 +41,19 @@ func _physics_process(_delta: float) -> void:
 				jump("1")
 		else:
 			if ($"../pipes".get_child_count()==0) and (not $front.is_colliding()) and (not $above.is_colliding()) and (not $"front-above".is_colliding()) and (not $below.is_colliding()) and (not $"front-below".is_colliding()):
+				print("q")
 				goingdown = false
 			if $"front-above".is_colliding() or $"front-below".is_colliding():
+				print("t")
 				if $"front-above".is_colliding():
+					print("w")
 					goingdown = true
 					diving("b")
-				else:
+				else: 
+					print("e")
+					goingdown = false
 					jump("2")
 			if(global_position.y >= randi_range(350,450)):
+				print("r")
 				jump("4")
 	move_and_slide()
