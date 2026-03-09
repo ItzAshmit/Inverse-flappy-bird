4bc8d9cfb05628b58cd89e13a443939b48972d95
smkillerz777-star <s.mkillerz777@gmail.com>
2026-03-09 16:54:50 +0530

    Tweak front RayCast and simplify player logic
    Increase front RayCast target_position from Vector2(75, 0) to Vector2(85, 0) to extend its reach. Simplify player.gd by removing the unused diving(out) function and debug prints, change jump to a parameterless function, and update all call sites to use jump() (remove diving calls). Clean up conditional branches and extraneous print statements to reduce noise and clarify control flow in _physics_process.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,14 +6,11 @@ var goingdown = false
 func _ready() -> void:
 	velocity =  Vector2.UP*speed
 
-func diving(out):
-	print(out)
-func jump(out):
+func jump():
 	if(goingdown or jumped): return
 	velocity.y = -speed
 	jumped = true
 	goingdown = false
-	print(out)
 
 func get_collider_layer(ray) -> int:
 	if ray.is_colliding():
@@ -25,35 +22,25 @@ func _physics_process(_delta: float) -> void:
 	velocity.y += gravity
 	if $below.is_colliding() or $below2.is_colliding() and (get_collider_layer($below) == 4 or get_collider_layer($below2) == 4):
 		goingdown=false
-		jump("3")
+		jump()
 	elif ($above.is_colliding() or $above2.is_colliding()) and (get_collider_layer($above) == 2 or get_collider_layer($above2) == 2):
 		goingdown=true
-		diving("c")
 	else:
-		print("k")
 		if $front.is_colliding():
-			print($front.get_collider())
 			if $front.get_collider().collision_layer == 2:
 				goingdown=true
-				diving("a")
 			else:
 				goingdown=false
-				jump("1")
+				jump()
 		else:
 			if ($"../pipes".get_child_count()==0) and (not $front.is_colliding()) and (not $above.is_colliding()) and (not $"front-above".is_colliding()) and (not $below.is_colliding()) and (not $"front-below".is_colliding()):
-				print("q")
 				goingdown = false
 			if $"front-above".is_colliding() or $"front-below".is_colliding():
-				print("t")
 				if $"front-above".is_colliding():
-					print("w")
 					goingdown = true
-					diving("b")
 				else: 
-					print("e")
 					goingdown = false
-					jump("2")
+					jump()
 			if(global_position.y >= randi_range(350,450)):
-				print("r")
-				jump("4")
+				jump()
 	move_and_slide()
