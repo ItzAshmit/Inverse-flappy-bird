8eb82daf7e47ec5866742a110554306bd488ee63
Ashmit here <ashmitagnihotrihero@gmail.com>
2026-03-10 20:42:31 +0530

    added level scenes

--- Scripts/level_menu.gd
+++ Scripts/level_menu.gd
@@ -1,15 +1,8 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main_game.tscn")
+	var button := get_viewport().gui_get_focus_owner()
+	if button:
+		get_tree().change_scene_to_file("res://Scenes/levels/" + str(button.get_parent().name) + ".tscn")
+		print(button.get_parent().name)
