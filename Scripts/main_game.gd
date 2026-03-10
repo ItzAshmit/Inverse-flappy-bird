49693eec1026e1e60c99e45c735c500ef3dcee50
Ashmit here <ashmitagnihotrihero@gmail.com>
2026-03-10 13:26:52 +0530

    added a boundary and a area to place pipes

--- Scripts/main_game.gd
+++ Scripts/main_game.gd
@@ -3,12 +3,6 @@ func pipe_spawner() -> void:
 	var Pipe_scene = load("res://Scenes/pipe.tscn")
 	var pipe = Pipe_scene.instantiate()
 	pipe.global_position = get_global_mouse_position()
-	if pipe.global_position.x< 380: 
-		pipe.global_position.x=380
-	elif pipe.global_position.x> 440: 
-		pipe.global_position.x=440
-	else:
-		pass
 	if pipe.global_position.y<310:
 		pipe.get_node("Upper").queue_free()
 		$pipes.add_child(pipe)           
@@ -20,5 +14,7 @@ func pipe_spawner() -> void:
 	
 func _input(event):
 	if event is InputEventMouseButton and $"Timer/PipeSpawner".time_left==0:
+		if get_global_mouse_position().x < 550:
+			return
 		$Timer/PipeSpawner.start()
 		pipe_spawner()
