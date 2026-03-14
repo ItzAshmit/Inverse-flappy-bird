b27fabbcd41ae3b44d72e5bcafbc8dffeea3b1dd
Ashmit here <ashmitagnihotrihero@gmail.com>
2026-03-14 16:31:18 +0530

    fixed levels files

--- Scripts/level_menu.gd
+++ Scripts/level_menu.gd
@@ -4,4 +4,4 @@ extends Control
 func _on_button_pressed() -> void:
 	var button := get_viewport().gui_get_focus_owner()
 	if button:
-		get_tree().change_scene_to_file("res://Scenes/levels/" + str(button.get_parent().name) + ".tscn")
+		get_tree().change_scene_to_file("res://Scenes/levels/" + (str(button.get_parent().name)) + ".tscn")
