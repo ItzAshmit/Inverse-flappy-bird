cb9ae2383af3c8bf91c8f57b2d78e477700b18ee
Ashmit here <ashmitagnihotrihero@gmail.com>
2026-03-14 22:43:52 +0530

    cooldown working

--- Scenes/levels/cooldown.gd
+++ Scenes/levels/cooldown.gd
@@ -1,23 +1,29 @@
 extends Sprite2D
 
 @export var cooldown:float = 4.0
+@onready var pipe_spawner: Timer = $"../../Timer/PipeSpawner"
 
 var step:float
 
 func _ready():
+	cooldown = pipe_spawner.wait_time
 	step = cooldown / 8
 	frame = 0
-	timer()
 
 
-	
+func _process(_delta):
+	if pipe_spawner.wait_time - pipe_spawner.time_left  != 1:
+		print(pipe_spawner.wait_time - pipe_spawner.time_left)
+		if step * frame <= (pipe_spawner.wait_time - pipe_spawner.time_left):
+			if frame <= 6:
+				frame += 1
 
-var time = 0
-func timer():
-	while time < cooldown:
-		if step * frame <= time:
-			frame += 1
-			if frame == 7:
-				frame = 0
-		time += 0.1
+
+func _on_pipe_spawner_timeout() -> void:
+	for i in range(-7,1):
+		if not frame == 0:
+			frame = i * -1
+		else:
+			frame = 0
+			print("k")
 		await get_tree().create_timer(0.1).timeout
