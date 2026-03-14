19870dd8df576b5562eab520f8dad7ae4a6dd251
Ashmit here <ashmitagnihotrihero@gmail.com>
2026-03-14 18:58:53 +0530

    added blured effect to disabled levels

--- Scripts/level_menu.gd
+++ Scripts/level_menu.gd
@@ -1,14 +1,23 @@
 extends Control
 var levels_cleared = 0
 
+
+func _ready():
+	load_data()
+	for i in $Levels.get_children():
+		var level_num = str(i.name).substr(5,-1).to_int()
+		if not (level_num <= (levels_cleared + 1)):
+			i.modulate = Color(1.0, 1.0, 1.0, 0.3)
+
+
+
 func _on_button_pressed() -> void:
 	var button := get_viewport().gui_get_focus_owner()
 	if button:
-		load_data()
 		var level = str(button.get_parent().name).substr(5,-1).to_int()
 		if(level <= (levels_cleared + 1)):
 			get_tree().change_scene_to_file("res://Scenes/levels/" + str(button.get_parent().name) + ".tscn")
-			
+
 func load_data():
 	var config = ConfigFile.new()
 	var save_path = "user://save.cfg"
