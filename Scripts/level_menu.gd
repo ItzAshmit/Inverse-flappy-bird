5affb376c4dddaa971745a1c3f959993d0d6f7b4
smkillerz777-star <s.mkillerz777@gmail.com>
2026-03-14 16:50:41 +0530

    djls

--- Scripts/level_menu.gd
+++ Scripts/level_menu.gd
@@ -1,19 +1,6 @@
 extends Control
-var levels_cleared =0
 
 func _on_button_pressed() -> void:
 	var button := get_viewport().gui_get_focus_owner()
 	if button:
-		load_data()
-		var level = str(button.get_parent().name).substr(5,-1).to_int()
-		if(level<=(levels_cleared+1)):
 			get_tree().change_scene_to_file("res://Scenes/levels/" + str(button.get_parent().name) + ".tscn")
-func load_data():
-	var config = ConfigFile.new()
-	var save_path = "user://save.cfg"
-	var err = config.load("user://save.cfg")
-	if err == 0:
-		levels_cleared = config.get_value("progress", "levels_cleared", 0)
-	else:
-		config.set_value("progress", "levels_cleared", 0)
-		config.save(save_path)
