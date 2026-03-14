9102c688c56a9fc06a559d818c0bbc8345a5d40f
smkillerz777-star <s.mkillerz777@gmail.com>
2026-03-14 15:47:27 +0530

    Add UI button and tighten pipe spawner logic
    Add a Buttons Control and a toggle Button to level1.tscn (on-screen "clickme" button). Refactor Scripts/main_game.gd: change pipe_spawner() to return an int status, restrict spawning to x within (380,440), nest the vertical (Upper/Lower) removal logic inside that x-range, and return 1 on successful spawn (0 otherwise). Update _input to only start the PipeSpawner timer when pipe_spawner() indicates a spawn occurred. Also preserve existing health/UI setup.

--- Scripts/main_game.gd
+++ Scripts/main_game.gd
@@ -6,29 +6,26 @@ func _ready() -> void:
 		$BirdParent.add_child(bird)
 	$CanvasLayer/health.scale.x = $CanvasLayer/health.scale.x*$BirdParent.get_child(0).maxhealth/100
 	$CanvasLayer/health/healthline.pivot_offset = Vector2(14.14,18.8)
-func pipe_spawner() -> void:
+func pipe_spawner() -> int:
 	var Pipe_scene = load("res://Scenes/pipe.tscn")
 	var pipe = Pipe_scene.instantiate()
 	pipe.global_position = get_global_mouse_position()
-	if pipe.global_position.x< 380: 
-		pipe.global_position.x=380
-	elif pipe.global_position.x> 440: 
-		pipe.global_position.x=440
-	else:
-		pass
-	if pipe.global_position.y<310:
-		pipe.get_node("Upper").queue_free()
-		$pipes.add_child(pipe)           
-	elif pipe.global_position.y>390: 
-		pipe.get_node("Lower").queue_free()
-		$pipes.add_child(pipe)   
-	else:
-		$pipes.add_child(pipe)
+	if pipe.global_position.x> 380 and pipe.global_position.x< 440: 
+		if pipe.global_position.y<310:
+			pipe.get_node("Upper").queue_free()
+			$pipes.add_child(pipe)           
+		elif pipe.global_position.y>390: 
+			pipe.get_node("Lower").queue_free()
+			$pipes.add_child(pipe)   
+		else:
+			$pipes.add_child(pipe)
+		return 1
+	return 0
 	
 func _input(event):
 	if event is InputEventMouseButton and $"Timer/PipeSpawner".time_left==0:
-		$Timer/PipeSpawner.start()
-		pipe_spawner()
+		if(pipe_spawner()==1):
+			$Timer/PipeSpawner.start()
 
 func _process(_delta: float) -> void:
 	$CanvasLayer/health/healthline.scale.x = $BirdParent.get_child(0).health/100
