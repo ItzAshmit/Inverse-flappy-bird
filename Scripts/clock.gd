66502af1544d84ed49af4cf2466e529040742f8b
Ashmit here <ashmitagnihotrihero@gmail.com>
2026-03-16 21:58:55 +0530

    kardiya push

--- Scripts/clock.gd
+++ Scripts/clock.gd
@@ -4,6 +4,7 @@ signal finished()
 
 
 func start_clock(time):
+	modulate = Color(1.0,1.0,1.0,1.0)
 	var tween = create_tween()
 	tween.tween_property($hand, "rotation_degrees", 360, time)
 	await tween.finished
