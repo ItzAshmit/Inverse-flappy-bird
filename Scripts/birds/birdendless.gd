25bc5cfe5c85dffac6a71890b3289bd96025cccd
smkillerz777-star <s.mkillerz777@gmail.com>
2026-03-21 18:45:49 +0530

    made the health bar invisible

--- Scripts/birds/birdendless.gd
+++ Scripts/birds/birdendless.gd
@@ -4,5 +4,6 @@ var clock
 func _ready() -> void:
 	super()
 	clock = self.get_parent().get_parent().get_node("CanvasLayer/clock")
+	$CanvasLayer/TextureProgressBar.visible = false
 func damage(_pipe):
 	clock.time_left += time
